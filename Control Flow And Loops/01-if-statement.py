bc441ef2968aeb31d13cee9da0991bdc4221d2f8
Rahil Mulani <141037000+rahilmulani025@users.noreply.github.com>
2025-09-06 22:54:45 +0530

    Delete Control Flow And Loops directory

--- Control Flow And Loops/01-if-statement.py
+++
@@ -1,7 +0,0 @@
-age = 12
-
-if(age>18):
-    print("You can drive")
-    print("Thank you ")
-
-print("End of program")
--- Control Flow And Loops/02-if-else.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter your age: "))
-
-if(age>18):
-    print("You can drive")
-else:
-    print("You cannot drive")
-
-print("End of program")
